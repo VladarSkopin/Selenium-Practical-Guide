10ff2edab2a3da421788d9f6e34ac06ebdf5514d
VladarSkopin <grudagor@gmail.com>
2022-11-14 14:24:53 +0300

    script for FirefoxDriver

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,37 @@
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class Main {
     public static void main(String[] args) {
-        System.setProperty("webdriver.gecko.driver","D:\\SeleniumProjectsQA\\geckodriver.exe");
-        WebDriver driver = new FirefoxDriver();
 
-        driver.get("http://www.google.com");
+        System.out.println("setting property...");
+        System.setProperty("webdriver.gecko.driver","D:\\SeleniumProjectsQA\\geckodriver.exe");
+
+        System.out.println("setting DesiredCapabilities...");
+        DesiredCapabilities dc = new DesiredCapabilities();
+        //DesiredCapabilities dc = new DesiredCapabilities().firefox();
+        dc.setPlatform(Platform.ANY);
+        dc.setCapability("marionatte", false);
+
+        System.out.println("merging FirefoxOptions...");
+        FirefoxOptions opt = new FirefoxOptions();
+        opt.merge(dc);
+
+        System.out.println("initializing FirefoxDriver with options...");
+        WebDriver driver = new FirefoxDriver(opt);
+
+        System.out.println("maximizing window...");
+        driver.manage().window().maximize();
+
+        System.out.println("going to google web page...");
+        driver.get("https://www.google.com/");
+
+        System.out.println("Application opened");
+        System.out.println("Page Title is : "+driver.getTitle());
+        //driver.quit();
     }
 }
 
